32030061ebc0b05a3d24f33f849e79ef5ed4da62
Joshua Shabo <josh.shabo@abex.capital>
2023-07-19 22:18:24 +0100

    full project implementation with review comments and suggestions

--- intropyproject-classify-pet-images/get_input_args.py
+++ intropyproject-classify-pet-images/get_input_args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/get_input_args.py
-#                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                   
-# REVISED DATE: 
-# PURPOSE: Create a function that retrieves the following 3 command line inputs 
-#          from the user using the Argparse Python module. If the user fails to 
+#
+# PROGRAMMER: Joshua Shabo
+# DATE CREATED: 16/07/2023
+# REVISED DATE: 19/07/2023
+# PURPOSE: Create a function that retrieves the following 3 command line inputs
+#          from the user using the Argparse Python module. If the user fails to
 #          provide some or all of the 3 inputs, then the default values are
 #          used for the missing inputs. Command Line Arguments:
 #     1. Image Folder as --dir with default value 'pet_images'
@@ -14,20 +14,32 @@
 #     3. Text File with Dog Names as --dogfile with default value 'dognames.txt'
 #
 ##
-# Imports python modules
 import argparse
+import os
 
-# TODO 1: Define get_input_args function below please be certain to replace None
-#       in the return statement with parser.parse_args() parsed argument 
-#       collection that you created with this function
-# 
+def valid_dir(path):
+    if not os.path.isdir(path):
+        raise argparse.ArgumentTypeError(f"readable_dir:{path} is not a valid path")
+    if os.access(path, os.R_OK):
+        return path
+    else:
+        raise argparse.ArgumentTypeError(f"readable_dir:{path} is not a readable dir")
+
+def valid_file(path):
+    if not os.path.isfile(path):
+        raise argparse.ArgumentTypeError(f"{path} is not a valid file")
+    if os.access(path, os.R_OK):
+        return path
+    else:
+        raise argparse.ArgumentTypeError(f"{path} is not readable")
+
 def get_input_args():
     """
     Retrieves and parses the 3 command line arguments provided by the user when
-    they run the program from a terminal window. This function uses Python's 
-    argparse module to created and defined these 3 command line arguments. If 
-    the user fails to provide some or all of the 3 arguments, then the default 
-    values are used for the missing arguments. 
+    they run the program from a terminal window. This function uses Python's
+    argparse module to created and defined these 3 command line arguments. If
+    the user fails to provide some or all of the 3 arguments, then the default
+    values are used for the missing arguments.
     Command Line Arguments:
       1. Image Folder as --dir with default value 'pet_images'
       2. CNN Model Architecture as --arch with default value 'vgg'
@@ -36,8 +48,13 @@ def get_input_args():
     Parameters:
      None - simply using argparse module to create & store command line arguments
     Returns:
-     parse_args() -data structure that stores the command line arguments object  
+     parse_args() -data structure that stores the command line arguments object
     """
-    # Replace None with parser.parse_args() parsed argument collection that 
-    # you created with this function 
-    return None
+
+    parser = argparse.ArgumentParser(prog="CNN Image Classifier")
+
+    parser.add_argument('--dir', help = 'a valid path to the folder of pet images', default = 'pet_images/', type = valid_dir)
+    parser.add_argument('--dogfile', help = 'a valid path to the file containing dognames', default = 'dognames.txt', type = valid_file)
+    parser.add_argument('--arch', help = 'the CNN model architecture for the classifier', default = 'vgg', choices = ['vgg', 'alexnet', 'resnet'])
+
+    return parser.parse_args()
